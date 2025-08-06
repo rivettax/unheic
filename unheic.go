a87e6b3fec2c466bbed930a3b9d976cf81d4b733
Wilson Hobbs <wilson@rivet.tax>
2025-08-06 16:37:46 -0700

    add base URL option, refactor to accept params object

--- unheic.go
+++ unheic.go
@@ -10,20 +10,42 @@ import (
 
 type Client struct {
 	httpClient *http.Client
+	baseURL    string
+}
+
+type ClientParams struct {
+	// BaseURL is the base URL of the unheic server. Default is http://localhost:8080
+	BaseURL string
+
+	// HTTPClient is the HTTP client to use. Default is http.Client{}
+	HTTPClient *http.Client
 }
 
 var ErrBadRequest = errors.New("bad request")
 
-func NewClient(httpClient *http.Client) *Client {
-	return &Client{httpClient: httpClient}
+func NewClient(params ClientParams) *Client {
+	if params.HTTPClient == nil {
+		params.HTTPClient = &http.Client{}
+	}
+
+	if params.BaseURL == "" {
+		params.BaseURL = "http://localhost:8080"
+	}
+
+	return &Client{
+		httpClient: params.HTTPClient,
+		baseURL:    params.BaseURL,
+	}
 }
 
 func (c *Client) Convert(ctx context.Context, src io.Reader) (io.Reader, error) {
-	req, err := http.NewRequestWithContext(ctx, "POST", "http://localhost:8080/convert", src)
+	req, err := http.NewRequestWithContext(ctx, "POST", c.baseURL+"/convert", src)
 	if err != nil {
 		return nil, err
 	}
+
 	req.Header.Set("Content-Type", "image/heic")
+
 	resp, err := c.httpClient.Do(req)
 	if err != nil {
 		return nil, err
